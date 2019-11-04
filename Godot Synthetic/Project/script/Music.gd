7c94eb549f4e401ca4d722572fd416f6a49145cc
Valter-Wulfric-Sherbune-Jr <hiiamaalien@gmail.com>
2019-11-04 13:45:06 -0500

    Update music

--- Godot Synthetic/Project/script/Music.gd
+++ Godot Synthetic/Project/script/Music.gd
@@ -3,16 +3,34 @@ extends Node
 
 #play(instrument, note, time_start, length)
 
-var ode_to_joy = ["E","E","F","G","G","F","E","D","C","C","D","E","E","D","D",
-  "E","E","F","G","G","F","E","D","C","C","D","E","D","C","C",
-   "D","D","E","C","D","E","F","E","C","D","E","F","E","D","C","D",
-  "E","E","F","G","G","F","E","D","C","C","D","E","D","C","C"]
+
+var dark_horse_beat = ["C5","C5","C5","C5","B","B","A","E",]
 
 # Called when the node enters the scene tree for the first time.
 func play():
-	
-	var i = 2
-	for note in ode_to_joy:
-		AudioManager.play("Cello", note, i, 1)
-		AudioManager.play("Drum", note, i, 1)
-		i+= .5
+    
+#      for x in range(0,3):
+#        example_1()
+
+      for x in range(1,3):
+        example_2()
+    
+
+func example_1():
+    AudioManager.play("Cello", "C5", 0, 1)
+    AudioManager.play("Cello", "C5", 1.1, 1)
+    AudioManager.play("Cello", "C5", 2.1, 1)
+    AudioManager.play("Cello", "C5", 3.1, 1)
+    AudioManager.play("Cello", "B", 4.1, 1)
+    AudioManager.play("Cello", "B", 5.1, 1)
+    AudioManager.play("Cello", "A", 6.1, 1)
+    AudioManager.play("Cello", "E", 7.1, 1)
+    
+    
+func example_2():
+    
+    var StartNoteTime = 0; #starts the notes and song at 5 seconds
+    
+    for note in dark_horse_beat:
+        AudioManager.play("Cello", note, StartNoteTime, .7)
+        StartNoteTime += .75 #increases the start time of the next note by .5 seconds
