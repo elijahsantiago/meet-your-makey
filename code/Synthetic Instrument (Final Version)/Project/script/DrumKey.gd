71c698d06f6270ed0791c4e850f0e55159ba6e1a
ELIJAH SANTIAGO <asantiago7@ggc.edu>
2025-04-09 06:44:09 -0400

    Polishing Touches + All Instruements
    Overhaul

--- code/Synthetic Instrument (Final Version)/Project/script/DrumKey.gd
+++ code/Synthetic Instrument (Final Version)/Project/script/DrumKey.gd
@@ -7,46 +7,43 @@ var key_value
 
 var player
 var is_pressed = false
-var timer_t = null
 
 var shrink_timer: Timer
 var reset_timer: Timer  
 
+var original_scale := Vector2.ONE  # This makes them dynamic from scene
+
 func _ready():
+    original_scale = rect_scale
+
     # Initialize timers
     shrink_timer = Timer.new()
-    self.add_child(shrink_timer)
+    add_child(shrink_timer)
     shrink_timer.connect("timeout", self, "_on_shrink_timeout")
     
     reset_timer = Timer.new()
-    self.add_child(reset_timer)
+    add_child(reset_timer)
     reset_timer.connect("timeout", self, "_on_reset_timeout")
-    
-    self.rect_scale = Vector2(1, 1)  # Ensure sprite starts with normal scale
 
 func _process(delta):
     if is_pressed:
-        # Shrink the sprite when pressed
-        self.rect_scale = Vector2(0.9, 0.9)
+        rect_scale = original_scale * 0.9  # Shrink based on original scale
         shrink_timer.start(0.05)
     else:
-        self.rect_scale = Vector2(1, 1)
+        rect_scale = original_scale  # Reset to original scale
         reset_timer.stop()
         shrink_timer.stop()
 
     # Play the audio
-    if player != null and player.playing == true:
+    if player != null and player.playing:
         player.pitch_scale = float(AudioManager.pitch)
-        if AudioManager.volume == 0:
-            player.volume_db = -80
-        else:
-            player.volume_db = (((AudioManager.volume - 0) * (5 - (-20))) / (100 - 0)) + (-20)
+        player.volume_db = -80 if AudioManager.volume == 0 else (((AudioManager.volume - 0) * (5 - (-20))) / (100 - 0)) + (-20)
 
 func _play_music():
     is_pressed = true
     
     player = AudioStreamPlayer.new()
-    self.add_child(player)
+    add_child(player)
 
     key_value = Key_Identifier.keys()[key]
     player.stream = AudioManager.drum_dictionary[key_value]
@@ -54,17 +51,15 @@ func _play_music():
     player.pitch_scale = AudioManager.pitch
     player.play()
 
-    if AudioManager.is_recording == true:
+    if AudioManager.is_recording:
         AudioManager.ref_scroll_item_container._spawn("Drum", key_value, AudioManager.elapsed_mili, .1)
 
 func _stop_music():
-    # Stop playing audio and reset the effect
     is_pressed = false
-    self.rect_scale = Vector2(1, 1)  # Reset sprite scale to normal size
+    rect_scale = original_scale  # Reset to original (NEEDED TO KEEP THEM LIKE LAYOUT)
 
 func _on_shrink_timeout():
-    # Shrink the sprite temporarily
-    self.rect_scale = Vector2(0.9, 0.9)  # Shrink sprite while pressed
+    rect_scale = original_scale * 0.9
 
 func _on_reset_timeout():
-    self.rect_scale = Vector2(1, 1)  # Reset to normal size after effect
+    rect_scale = original_scale
